d2367213a414c532972de8320ee6686ee84ed7b3
杨再涛 <1772081518@qq.com>
2026-07-22 23:27:48 +0800

    标准化 WinMain 参数并标记未使用参数
    将 WinMain 的参数列表更改为标准的四个参数（hInstance, hPrevInstance, lpCmdLine, nCmdShow），并使用 UNREFERENCED_PARAMETER 宏显式标记未使用的参数，消除编译器未使用参数的警告。

--- main.cpp
+++ main.cpp
@@ -9,8 +9,12 @@
 
 static std::unique_ptr<Application> app;
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+    UNREFERENCED_PARAMETER(nCmdShow);
+
     SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
     HRESULT hr = CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
     if (FAILED(hr) && hr != RPC_E_CHANGED_MODE)
